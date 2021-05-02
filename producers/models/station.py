66b684a093a7f538525e3e00248d89a7086d2b27
xlsarath <xlsarathchandra@gmail.com>
2021-05-01 23:53:40 -0700

    assigned partition and other config for kafka msg producer for stations

--- producers/models/station.py
+++ producers/models/station.py
@@ -19,7 +19,7 @@ class Station(Producer):
     #
     # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
     #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
@@ -31,19 +31,15 @@ class Station(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of replicas
+                
+        topic_name = f"{chicago_trains_arrival}" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
+            num_partitions=2,
+            um_replicas=1,
         )
 
         self.station_id = int(station_id)
@@ -57,23 +53,24 @@ class Station(Producer):
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
+        
         # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        try:
+            self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id" : self.station_id,
+                    "train_id" : train.train_id,
+                    "direction" : direction,
+                    "line" :  self.color.name,
+                    "train_status" : train.status.name,
+                    "prev_station_id" : prev_station_id,
+                    "prev_direction" : prev_direction,
+                },
+            )
+        except Exception as e:
+            logger.error(f"arrival kafka integration incomplete - skipping {str(e)}")
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
